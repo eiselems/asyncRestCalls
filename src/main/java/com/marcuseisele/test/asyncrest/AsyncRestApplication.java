60ddd1c6534cbf09b29d9391f157cb0a3ea3f692
Marcus Eisele <marcus.eisele@gmail.com>
2018-04-02 23:47:32 +0200

    INIT: from start.spring.io

---
+++ src/main/java/com/marcuseisele/test/asyncrest/AsyncRestApplication.java
@@ -0,0 +1,12 @@
+package com.marcuseisele.test.asyncrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncRestApplication.class, args);
+	}
+}
